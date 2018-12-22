aa54e262c1541db32e84356970f098fb5d28f42d
gustavo maurizio <gustavo,maurizio@gmail.com>
2018-12-22 09:32:55 -0500

    Sat Dec 22 09:32:55 EST 2018

--- plugin_disk.go
+++ plugin_disk.go
@@ -1,38 +1,74 @@
 package main
 
 import (
-	"encoding/json"
-	"errors"
-	"fmt"
-	"github.com/shirou/gopsutil/disk"
-	log "github.com/sirupsen/logrus"
-    	"time"
+		"encoding/json"
+		"errors"
+		"fmt"
+		"github.com/shirou/gopsutil/disk"
+		log "github.com/sirupsen/logrus"
+		"github.com/prometheus/client_golang/prometheus"
+		"github.com/prometheus/client_golang/prometheus/promhttp"
+		"net/http"		
+		"time"
 )
 
-var PluginEnv		[]disk.PartitionStat
+//	Define the metrics we wish to expose
+var diskioIndicator = prometheus.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "sreagent_disk_iop",
+		Help: "Disk IO",
+	}, []string{"disk","operation"} )
+
+var diskbIndicator = prometheus.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "sreagent_disk_bytes",
+		Help: "Disk Bytes",
+	}, []string{"disk","operation"} )
+
+
 var PluginConfig 	map[string]map[string]map[string]interface{}
-var PluginData		map[string]interface{}
+var PluginData 		map[string]interface{}
+
 
 
 func PluginMeasure() ([]byte, []byte, float64) {
-	// Get measurement of Disks
-	for k := range PluginData {delete(PluginData, k)}
-	d, _  := disk.Partitions(false)
-	for _, part := range d {
-		PluginData[part.Mountpoint], _ = disk.Usage(part.Mountpoint)
-	}
-	// Make it understandable
-	// Apply USE methodology for DISK
-	// U: 	Usage (usually throughput/latency indicators)
-	//		In this case we define as CPU average utilization 0-100
-	//		Latency is the MHz of each CPU weighted by usage
-	// S:	Saturation (how full is the filesystem)
-	// E:	Errors (not applicable for DISK)
-	// Prepare the data
+	// Get measurement of IOCounters
+	PluginData["ts_current"]		= time.Now().UnixNano()
+	PluginData["io_current"], _ 	= disk.IOCounters()
+	
+	Δts := PluginData["ts_current"].(int64) - PluginData["ts_previous"].(int64)		// nanoseconds!
+	
+	DISKS:
+	for diskid, _ := range PluginData["io_current"].(map[string]disk.IOCountersStat) {
+		_, present := PluginData["io_previous"].(map[string]disk.IOCountersStat)[diskid]
+		if !present {continue DISKS}
+		inc_riop 	:= PluginData["io_current"].(map[string]disk.IOCountersStat)[diskid].ReadCount  - PluginData["io_previous"].(map[string]disk.IOCountersStat)[diskid].ReadCount
+		inc_wiop 	:= PluginData["io_current"].(map[string]disk.IOCountersStat)[diskid].WriteCount - PluginData["io_previous"].(map[string]disk.IOCountersStat)[diskid].WriteCount
+		inc_rbytes 	:= PluginData["io_current"].(map[string]disk.IOCountersStat)[diskid].ReadBytes  - PluginData["io_previous"].(map[string]disk.IOCountersStat)[diskid].ReadBytes
+		inc_wbytes 	:= PluginData["io_current"].(map[string]disk.IOCountersStat)[diskid].WriteBytes - PluginData["io_previous"].(map[string]disk.IOCountersStat)[diskid].WriteBytes
 
-	myMeasure, _				:= json.Marshal(PluginData)
-	myMeasureRaw, _ 			:= json.Marshal(PluginData)
-	return myMeasure, myMeasureRaw, float64(time.Now().UnixNano())/1e9
+		// Update metrics related to the plugin
+		diskioIndicator.WithLabelValues(diskid, "read").Add(float64(inc_riop))
+		diskioIndicator.WithLabelValues(diskid, "write").Add(float64(inc_wiop))
+		diskbIndicator.WithLabelValues(diskid,  "read").Add(float64(inc_rbytes))
+		diskbIndicator.WithLabelValues(diskid, 	"write").Add(float64(inc_wbytes))
+
+		PluginData["io"] = map[string]map[string]float64	{
+			diskid: {
+				"riops":		float64(inc_riop)/float64(Δts) * 1e9,
+				"wiops":		float64(inc_wiop)/float64(Δts) * 1e9,
+				},
+		}
+
+		PluginData["mbps"] = map[string]map[string]float64	{
+			diskid: {
+				"rmbps":		float64(inc_rbytes)/float64(Δts) * 1e3,
+				"wmbps":		float64(inc_wbytes)/float64(Δts) * 1e3,
+			},
+		}
+	}
+	myMeasure, _ := json.Marshal(PluginData)
+	return myMeasure, []byte(""), float64(PluginData["ts_current"].(int64)) / 1e9
 }
 
 func PluginAlert(measure []byte) (string, string, bool, error) {
@@ -40,58 +76,76 @@ func PluginAlert(measure []byte) (string, string, bool, error) {
 	// var m 			interface{}
 	// err := json.Unmarshal(measure, &m)
 	// if err != nil { return "unknown", "", true, err }
-	alertMsg  := ""
-	alertLvl  := ""
+	alertMsg := ""
+	alertLvl := ""
 	alertFlag := false
-	alertErr  := errors.New("no error")
+	alertErr := errors.New("no error")
 
 	return alertMsg, alertLvl, alertFlag, alertErr
 }
 
-
-func InitPlugin(config string) () {
+func InitPlugin(config string) {
 	if PluginData == nil {
-		PluginData = make(map[string]interface{},20)
+		PluginData = make(map[string]interface{}, 20)
 	}
 	if PluginConfig == nil {
-		PluginConfig = make(map[string]map[string]map[string]interface{},20)
+		PluginConfig = make(map[string]map[string]map[string]interface{}, 20)
 	}
-	PluginEnv, _	= disk.Partitions(false)
 	err := json.Unmarshal([]byte(config), &PluginConfig)
 	if err != nil {
 		log.WithFields(log.Fields{"config": config}).Error("failed to unmarshal config")
 	}
-	log.WithFields(log.Fields{"pluginconfig": PluginConfig, "pluginenv": PluginEnv }).Info("InitPlugin")
+	PluginData["ts_previous"]	 	= time.Now().UnixNano()
+	PluginData["io_previous"], _ 	= disk.IOCounters()
+	// Register metrics with prometheus
+	prometheus.MustRegister(diskioIndicator)
+	prometheus.MustRegister(diskbIndicator)
+
+	log.WithFields(log.Fields{"pluginconfig": PluginConfig, "plugindata": PluginData}).Info("InitPlugin")
 }
 
-
 func main() {
-	// for testing purposes only, can safely not exist!
-	// config := " { \"alert\": {    \"blue\":   [0,  3], \"green\":  [3,  60], \"yellow\": [60, 80], \"orange\": [80, 90], \"red\":    [90, 100] } } "
 	config  := 	`
-				{"alert": { 
-				            "/": 		{"low": 10, "design": 60.0, "engineered": 80.0},
-				            "/opt":		{"low": 05, "design": 40.0, "engineered": 70.0}
-				          }
+				{
+					"alert": 
+					{
+						"cpu":
+						{
+							"low": 			2,
+							"design": 		60.0,
+							"engineered":	80.0
+						}
+				    }
 				}
 				`
+
+	//--------------------------------------------------------------------------//
+	// time to start a prometheus metrics server
+	// and export any metrics on the /metrics endpoint.
+	http.Handle("/metrics", promhttp.Handler())
+	go func() {
+		http.ListenAndServe(":8999", nil)
+	}()
+	//--------------------------------------------------------------------------//
+
 	InitPlugin(config)
 	log.WithFields(log.Fields{"PluginConfig": PluginConfig}).Info("InitPlugin")
-	tickd := 1* time.Second
-	for i := 1; i <= 2; i++ {
+	tickd := 1 * time.Second
+	for i := 1; i <= 100; i++ {
 		tick := time.Now().UnixNano()
 		measure, measureraw, measuretimestamp := PluginMeasure()
 		alertmsg, alertlvl, isAlert, err := PluginAlert(measure)
-		fmt.Printf("Iteration #%d tick %d \n", i, tick)
-		log.WithFields(log.Fields{"timestamp": measuretimestamp, 
-					  "measure": string(measure[:]),
-					  "measureraw": string(measureraw[:]),
-					  "PluginData": PluginData,
-					  "alertMsg": alertmsg,
-					  "alertLvl": alertlvl,
-					  "isAlert":  isAlert,
-					  "AlertErr":      err,
-		}).Info("Tick")
+		fmt.Printf("Iteration #%d tick %d %v\n", i, tick,PluginData["io"].(map[string]map[string]float64),
+														 PluginData["mbps"].(map[string]map[string]float64))
+		log.WithFields(log.Fields{"timestamp": measuretimestamp,
+			"measure":    string(measure[:]),
+			"measureraw": string(measureraw[:]),
+			"PluginData": PluginData,
+			"alertMsg":   alertmsg,
+			"alertLvl":   alertlvl,
+			"isAlert":    isAlert,
+			"AlertErr":   err,
+		}).Debug("Tick")
 		time.Sleep(tickd)
 	}
 }
